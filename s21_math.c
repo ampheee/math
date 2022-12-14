c2dda514419520c00f2db88c25711da35ea87d46
Babbling Madelaine <babbling@mi-m6.msk.21-school.ru>
2022-12-14 17:00:24 +0300

    1

---
+++ s21_math.c
@@ -0,0 +1,59 @@
+#include "s21_math.h"
+
+#DEFINE exp = 1
+
+int main() {
+
+}
+
+int s21_abs(int x) {
+    return x < 0 ? x * -1 : x;
+}
+
+long double s21_acos(double x) {
+
+}
+
+long double s21_asin(double x) {
+
+}
+
+long double s21_atan(double x) {
+
+}
+
+long double s21_ceil(double x) {
+
+}
+
+long double s21_cos(double x) {
+
+}
+
+long double s21_exp(double x) {
+
+}
+
+long double s21_fabs(double x) {
+
+}
+
+long double s21_floor(double x) {
+
+}
+
+long double s21_fmod(double x, double y) {
+
+}
+
+long double s21_pow(double base, double exp) {
+
+}
+
+long double s21_sqrt(double x) {
+
+}
+
+long double s21_tan(double x) {
+    
+}
---
+++ s21_math.h
@@ -0,0 +1,4 @@
+#ifndef SRC_MATH_S21_MATH_H_
+#define SRC_MATH_S21_MATH_H_
+
+#include <check.h>
